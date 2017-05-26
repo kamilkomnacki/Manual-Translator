ad187103d360c24f79333c4643973073e83fe5f1
Kamil Komnacki <kamilkomnacki@gmail.com>
2017-05-26 21:19:46 +0200

    Initial commit

---
+++ app/src/main/java/com/komnacki/manualtranslator/WordsCatalogActivity.java
@@ -0,0 +1,13 @@
+package com.komnacki.manualtranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordsCatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words_catalog);
+    }
+}
