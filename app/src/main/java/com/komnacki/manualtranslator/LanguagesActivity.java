fa50557db9a08eb2dd14bb9fee661bf644d96408
Kamil Komnacki <kamilkomnacki@gmail.com>
2017-05-26 22:19:23 +0200

    First steps into build WordActivity

--- app/src/main/java/com/komnacki/manualtranslator/LanguagesActivity.java
+++ app/src/main/java/com/komnacki/manualtranslator/LanguagesActivity.java
@@ -1,7 +1,10 @@
 package com.komnacki.manualtranslator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LanguagesActivity extends AppCompatActivity {
@@ -11,6 +14,14 @@ public class LanguagesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_languages);
 
+        Button btn = (Button) findViewById(R.id.btn_wordActivity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WordActivity.class);
+                startActivity(intent);
 
+            }
+        });
     }
 }
